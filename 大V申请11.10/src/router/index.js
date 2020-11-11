28f86153b084c075a6ddce53091d5b375b2bcea5
webwang4312 <1873772924@qq.com>
2020-11-11 17:10:41 +0800

    测试

--- 大V申请11.10/src/router/index.js
+++ 大V申请11.10/src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
-const originalPush = VueRouter.prototype.push
-VueRouter.prototype.push = function push(location) {
-    return originalPush.call(this, location).catch(err => err)
-}
+// const originalPush = VueRouter.prototype.push
+// VueRouter.prototype.push = function push(location) {
+//     return originalPush.call(this, location).catch(err => err)
+// }
 // const routerPush = VueRouter.prototype.push
 // VueRouter.prototype.push = function push(location) {
 //   return routerPush.call(this, location).catch(error=> error)
